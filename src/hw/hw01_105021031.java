a4eed804cf375217ed6bae7480589426ca99d851
hukepasta0425 <mengjingzehuke@mengjingzede-MacBook-Pro.local>
2016-10-02 19:05:03 +0800

    10/2

--- src/hw/hw01_105021031.java
+++ src/hw/hw01_105021031.java
@@ -15,16 +15,26 @@ public class hw01_105021031 {
 		int c=src.nextInt();
 		
 		int x=a*15+b*20+c*30;
+		int p=0;
+		int p1=0;
+		int q=0;
+		int q1=0;
+		
 		if(n>=x)
 		{
-			
+			 p=x/50;
+			p1=x%50;
+			q=p1/5;
+			 q1=q%5;
 		}
 		else
 		{
 			System.out.print("0");
 		}
 		
-		
+		System.out.println(p+"個50元");
+		System.out.println(q+"個5元");
+		System.out.println(q1+"個1元");
 		
 	}
 
