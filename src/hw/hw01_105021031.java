83cedd8fd591ed90c8343d22b5a0acad3bf9abdd
hukepasta0425 <mengjingzehuke@mengjingzede-MacBook-Pro.local>
2016-10-02 18:17:50 +0800

    10/2

--- src/hw/hw01_105021031.java
+++ src/hw/hw01_105021031.java
@@ -5,7 +5,27 @@ public class hw01_105021031 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner src=new Scanner(System.in);
-	
+		System.out.print("錢： ");
+		int n=src.nextInt();
+		System.out.print("蘋果： ");
+		int a=src.nextInt();
+		System.out.print("柳丁： ");
+		int b=src.nextInt();
+		System.out.print("桃子： ");
+		int c=src.nextInt();
+		
+		int x=a*15+b*20+c*30;
+		if(n>=x)
+		{
+			
+		}
+		else
+		{
+			System.out.print("0");
+		}
+		
+		
+		
 	}
 
 }
--- src/hw/hw03_105021031.java
+++ src/hw/hw03_105021031.java
@@ -1,28 +1,36 @@
 package hw;
+
 import java.util.*;
+
 public class hw03_105021031 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner src =new Scanner(System.in);
+		Scanner src = new Scanner(System.in);
 		System.out.println("輸入西元年:");
-		int a=src.nextInt();
-		if(a%4==0&&a%100>0)
-		{
-			System.out.println("閏年!");
-		}
-		else if(a%400==0)
-		{
-			System.out.println("閏年!");
-		}
-		else if(a%1000==0)
-		{
-			System.out.println("閏年!");
-		}
+		int a = src.nextInt();
 		
-		else{
-			System.out.println("平年!");
+		if(a%4==0)
+		{
+			if(a%100==0)
+			{
+				if(a%400==0)
+				{
+					System.out.print("閏年");
+				}
+				else
+				{
+					System.out.print("平年");
+				}
+			}
+			else
+			{
+				System.out.print("閏年");
+			}
+		}
+		else
+		{
+			System.out.print("平年");
 		}
 	}
-
 }
